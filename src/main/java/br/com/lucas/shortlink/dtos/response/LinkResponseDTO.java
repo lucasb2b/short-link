dc28b00d5a25cb078913b7919f21959e1863223c
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2026-06-22 23:07:31 -0300

    fix: increase fields in LinkResponseDTO

--- src/main/java/br/com/lucas/shortlink/dtos/response/LinkResponseDTO.java
+++ src/main/java/br/com/lucas/shortlink/dtos/response/LinkResponseDTO.java
@@ -2,6 +2,8 @@ package br.com.lucas.shortlink.dtos.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.time.LocalDateTime;
+
 @Schema(description = "Resposta com os dados do link encurtado")
 public record LinkResponseDTO(
 
@@ -12,6 +14,12 @@ public record LinkResponseDTO(
         String shortUrl,
 
         @Schema(description = "Código único que representa o link curto", example = "abc123")
-        String shortCode
+        String shortCode,
+
+        @Schema(description = "Quantidade de acessos no link", example = "3")
+        long clicks,
+
+        @Schema(description = "Data de criação do link", example = "2026-04-24T10:34")
+        LocalDateTime createdAt
 ) {
 }
